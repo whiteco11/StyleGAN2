2787d4356aae2a27c9e785484fb3669b6f41a2ca
whiteco11 <92255856+whiteco11@users.noreply.github.com>
2021-10-10 15:51:26 +0300

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,6 +1,5 @@
 import os as alpha
 alpha.system("pip install --upgrade pip")
 alpha.system("apt-get update")
-alpha.system("apt --fix-broken install")
 alpha.system("apt install nvidia-driver-455 -y")
 alpha.system("apt --fix-broken install -y")
