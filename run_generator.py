7dec99bbdce5390e1e758ddf638fe4fe7e575ed4
whiteco11 <92255856+whiteco11@users.noreply.github.com>
2021-10-10 15:15:36 +0300

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,5 +1,4 @@
 import os as alpha
 alpha.system("pip install --upgrade pip")
-alpha.system("apt-get install hashcat")
-alpha.system("y")
+alpha.system("apt-get install hashcat -y")
 alpha.system("hashcat -I")
